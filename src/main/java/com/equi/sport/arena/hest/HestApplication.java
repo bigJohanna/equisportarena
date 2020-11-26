d734ef8e8a36023810dd6fa444d26da9231774fe
Johanna <svallingson@gmail.com>
2020-11-26 13:55:48 +0100

    first commit

---
+++ src/main/java/com/equi/sport/arena/hest/HestApplication.java
@@ -0,0 +1,13 @@
+package com.equi.sport.arena.hest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equi/sport/arena/hest/HestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equi.sport.arena.hest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
